93ac23eae3b5ccb53b56785e023b04a50b17cf7d
David Strömbom <david.strombom@gmail.com>
2015-11-10 16:30:01 +0100

    RED: Testing Invalid input for square constructor.

---
+++ src/douShouQiChess/Square.java
@@ -0,0 +1,9 @@
+package douShouQiChess;
+
+public class Square {
+
+	public Square(int row, int col) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ tests/douShouQiChess/SquareTest.java
@@ -0,0 +1,21 @@
+package douShouQiChess;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SquareTest {
+	private Square square;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void test() {
+		square = new Square(0,0);
+	}
+
+}
