77ff446646c34f5dfa189c1d0ae33dcd8bf4b51b
David Strömbom <david.strombom@gmail.com>
2015-11-10 16:32:34 +0100

    GREEN: Throws exception at 0 input for new square.

--- src/douShouQiChess/Square.java
+++ src/douShouQiChess/Square.java
@@ -3,7 +3,9 @@ package douShouQiChess;
 public class Square {
 
 	public Square(int row, int col) {
-		// TODO Auto-generated constructor stub
+		if(row == 0 || col == 0) {
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
