0ee2b148beb61cc6f8f6728a9fb13104c62fa6ad
David Strömbom <david.strombom@gmail.com>
2015-11-16 21:08:08 +0100

    GREEN: Added assign value method.

--- src/douShouQiChess/Piece.java
+++ src/douShouQiChess/Piece.java
@@ -11,6 +11,29 @@ public class Piece {
 	
 	public Piece(Animal animal) {
 		this.animal = animal;
+		this.value = this.assignValue();
+	}
+
+	private int assignValue() {
+		switch (this.animal) {
+		case RAT:
+			return 1;
+		case CAT:
+			return 2;
+		case WOLF:
+			return 3;
+		case DOG:
+			return 4;
+		case LEOPARD:
+			return 5;
+		case TIGER:
+			return 6;
+		case LION:
+			return 7;
+		case ELEPHANT:
+			return 8;
+		}
+		return 0;
 	}
 
 	public int getValue() {
