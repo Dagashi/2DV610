a6b0741549ee05f102077e18a06857953210411d
David Strömbom <david.strombom@gmail.com>
2015-11-23 14:49:45 +0100

    RED: Added test that uses mocking to check that the initialize method sets 16 pieces on the board.

---
+++ src/douShouQiChess/Game.java
@@ -0,0 +1,11 @@
+package douShouQiChess;
+
+public class Game {
+	Board board;
+	
+	public void initialize(Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ tests/douShouQiChess/GameTest.java
@@ -0,0 +1,23 @@
+package douShouQiChess;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Board mockedBoard = mock(Board.class);
+		Game game = new Game();
+		game.initialize(mockedBoard);
+		verify(mockedBoard, times(16)).setPieceAt(anyObject(), anyObject());
+	}
+
+}
