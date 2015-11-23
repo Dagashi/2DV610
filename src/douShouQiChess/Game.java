cb1878ad6c303796c112e83bb666f9f3df444793
David Strömbom <david.strombom@gmail.com>
2015-11-23 15:21:29 +0100

    GREEN: The method initialize sets 16 pieces on the board.

--- src/douShouQiChess/Game.java
+++ src/douShouQiChess/Game.java
@@ -1,11 +1,31 @@
 package douShouQiChess;
 
+import douShouQiChess.Piece.Animal;
+import douShouQiChess.Piece.Color;
+
 public class Game {
 	Board board;
 	
-	public void initialize(Board board) {
-		// TODO Auto-generated method stub
+	public void initialize(Board b) {
+		this.board = b;
 		
+		board.setPieceAt(new Square(1,1), new Piece(Animal.LION,Color.LIGHT));
+		board.setPieceAt(new Square(1,7), new Piece(Animal.TIGER,Color.LIGHT));
+		board.setPieceAt(new Square(2,2), new Piece(Animal.DOG,Color.LIGHT));
+		board.setPieceAt(new Square(2,6), new Piece(Animal.CAT,Color.LIGHT));
+		board.setPieceAt(new Square(3,1), new Piece(Animal.RAT,Color.LIGHT));
+		board.setPieceAt(new Square(3,3), new Piece(Animal.LEOPARD,Color.LIGHT));
+		board.setPieceAt(new Square(3,5), new Piece(Animal.WOLF,Color.LIGHT));
+		board.setPieceAt(new Square(3,7), new Piece(Animal.ELEPHANT,Color.LIGHT));
+		
+		board.setPieceAt(new Square(9,7), new Piece(Animal.LION,Color.DARK));
+		board.setPieceAt(new Square(9,1), new Piece(Animal.TIGER,Color.DARK));
+		board.setPieceAt(new Square(8,6), new Piece(Animal.DOG,Color.DARK));
+		board.setPieceAt(new Square(8,2), new Piece(Animal.CAT,Color.DARK));
+		board.setPieceAt(new Square(7,7), new Piece(Animal.RAT,Color.DARK));
+		board.setPieceAt(new Square(7,5), new Piece(Animal.LEOPARD,Color.DARK));
+		board.setPieceAt(new Square(7,3), new Piece(Animal.WOLF,Color.DARK));
+		board.setPieceAt(new Square(7,1), new Piece(Animal.ELEPHANT,Color.DARK));
 	}
 	
 }
